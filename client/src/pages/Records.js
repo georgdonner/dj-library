f0acc5b36bed2ad41d413c1d4bd1e1bbf9b5a0dd
Georg Donner <georgdonner@posteo.de>
2021-04-23 19:50:19 +0200

    Add basic search to records

--- client/src/pages/Records.js
+++ client/src/pages/Records.js
@@ -9,6 +9,7 @@ export default class Records extends Component {
     this.state = {
       page: props.records ? Math.floor(props.records.length / LIMIT) : -1,
       total: 0,
+      query: '',
     };
   }
 
@@ -18,19 +19,26 @@ export default class Records extends Component {
     }
   }
 
-  async fetchRecords() {
+  async fetchRecords(page) {
+    const newPage = page ?? (this.state.page + 1);
     const params = new URLSearchParams({
-      page: this.state.page + 1,
+      page: newPage,
       limit: LIMIT,
+      ...(this.state.query ? {q: this.state.query} : {}),
     });
 
     const res = await fetch('/api/records?' + params.toString());
     const {records, total} = await res.json();
     this.setState({
-      page: this.state.page + 1,
+      page: newPage,
       total,
     });
     return this.props.setRecords((this.props.records || []).concat(records));
+  }
+
+  async search() {
+    this.props.setRecords(null);
+    this.fetchRecords(0);
   }
 
   render() {
@@ -39,21 +47,45 @@ export default class Records extends Component {
     }
 
     return (
-      <div id="records" className="container mt-5">
-        {this.props.records.map((record) => (
-          <Link to={`/record/${record._id}`} key={record._id} className="media" style={{color: 'unset'}}>
-            <figure className="media-left">
-              <p className="image is-96x96">
-                <img src={record.coverImg} alt={record.title} />
-              </p>
-            </figure>
-            <div className="media-content">
-              <p><strong>{record.title}</strong></p>
-              <p>{record.artists.join(', ')}</p>
-              <p><small>{record.label} {record.year}</small></p>
-            </div>
-          </Link>
-        ))}
+      <div className="container my-5">
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <input
+              className="input" type="text"
+              placeholder="Filter by album name, artist or label"
+              value={this.state.query}
+              onChange={(e) => this.setState({
+                query: e.target.value,
+              })}
+              onKeyUp={(e) => {
+                if (e.key === 'Enter') {
+                  this.search();
+                }
+              }}
+            />
+          </div>
+          <div className="control">
+            <button className="button is-info" onClick={() => this.search()}>
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="mt-5">
+          {this.props.records.map((record) => (
+            <Link to={`/record/${record._id}`} key={record._id} className="media" style={{color: 'unset'}}>
+              <figure className="media-left">
+                <p className="image is-96x96">
+                  <img src={record.coverImg} alt={record.title} />
+                </p>
+              </figure>
+              <div className="media-content">
+                <p><strong>{record.title}</strong></p>
+                <p>{record.artists.join(', ')}</p>
+                <p><small>{record.label} {record.year}</small></p>
+              </div>
+            </Link>
+          ))}
+        </div>
         {this.state.total && this.state.total > this.props.records.length ? (
           <div className="is-justify-content-center">
             <button
